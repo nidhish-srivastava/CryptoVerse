ab24d9f3ee109c78728005885b9946ef13eb703e
Nidhish Srivastava <nidhish0430@gmail.com>
2023-07-13 12:10:01 +0530

    ..

--- src/components/HistoricalChart.tsx
+++ src/components/HistoricalChart.tsx
@@ -52,7 +52,6 @@ function HistoricalChart(){
                <Line data={{
                 labels:data?.prices?.map((coin) => {
                     let date = new Date(coin[0]);
-                    let time =
                       date.getHours() > 12
                         ? `${date.getHours() - 12}:${date.getMinutes()} PM`
                         : `${date.getHours()}:${date.getMinutes()} AM`;
