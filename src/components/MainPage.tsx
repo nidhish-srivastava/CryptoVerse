ddb71affd2faaebf5e49b0da94c25d9bcea3e6d9
Nidhish Srivastava <nidhish0430@gmail.com>
2023-07-17 00:03:59 +0530

    ..

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import Modal from "./Modal";
+// import Modal from "./Modal";
 
 export type apiDataType = {
   image: string;
