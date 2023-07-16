dd6be74797573faa13e3cbe73650d2377c478054
Nidhish Srivastava <nidhish0430@gmail.com>
2023-07-16 15:41:49 +0530

    Created the threee dots logic

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,6 +1,7 @@
 import React, { useState , useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
+import Modal from './Modal'
 
 export type apiDataType = {
   image : string
@@ -8,6 +9,8 @@ export type apiDataType = {
   id : string
   current_price : number 
   price_change_24h : string
+  market_cap_rank : number
+  hover ?: boolean
 }
 
 function MainPage() {
@@ -15,6 +18,9 @@ function MainPage() {
   const [apiData,setApiData] = useState<apiDataType[]>([])
   const [pageNumber, setPageNumber] = useState(1);
   const [filteredResults,setFilteredResults]=  useState<apiDataType[]>([])
+  const [showModal,setShowModal] = useState(false)
+
+  const navigate = useNavigate()
 
   const getData = async()=>{
     const response = await axios.get(
@@ -42,6 +48,39 @@ function MainPage() {
    for (let i = 1; i <= Math.ceil(250 / 50); i++) {
      pagNumberArray.push(i);
    }
+
+   const onMouseEnter = (index : number) =>{
+     setApiData(
+       apiData.map((e)=>{
+         if(e.market_cap_rank===index+1) {
+          if(!e.hover) e.hover=true
+          else e.hover =false
+          // console.log(e);
+        }
+        return e
+      })
+       )
+   }
+   const onMouseLeaver = (index : number) =>{
+     setApiData(
+       apiData.map((e)=>{
+         if(e.market_cap_rank===index+1) {
+          if(e.hover) e.hover=false
+          else e.hover = true
+          // console.log(e);
+        }
+        return e
+      })
+       )
+   }
+
+   const clickOnCardHandler = (id : string) =>{
+    navigate(`/${id}`)
+   }
+   const clickedOnSpan = (e : React.MouseEvent<HTMLDivElement, MouseEvent>)=>{
+    e.stopPropagation()
+    setShowModal(e=>!e)
+   }
   return (
     <React.Fragment>
      <div className="searchBarContainer">
@@ -58,33 +97,42 @@ function MainPage() {
       <div className="mainContainer">
         {searchInput.length > 1
           ? filteredResults.map((currEle, index) => {
-              const { image, name, id, current_price } = currEle;
+              const {image, name, id, current_price} = currEle;
               return (
-                <Link to={`/${id}`}>
-                  <div key={index} className="card">
-                    {/* <h2>{index+1}</h2> */}
+                <Link to={`/${id}`} key={index}>
+                  <div  className="card">
                     <img src={image} alt="" width="100px" />
                     <div className="details">
                       <h2>{name}</h2>
                       <h3>Current Price:₹{current_price}</h3>
                     </div>
+                    {/* {hover && <span className='three-dots'>
+                    <i className="fa-solid fa-ellipsis-vertical"></i>
+                    </span>
+                    } */}
                   </div>
                 </Link>
               );
             })
           : apiData.map((currEle, index) => {
-              const { image, name, id, current_price } = currEle;
+              const { image, name, id, current_price,hover } = currEle;
               return (
-                <Link to={`/${id}`}>
-                  <div key={index} className="card">
+                <div onClick = {()=>clickOnCardHandler(id)} key={index}>
+                  <div  className="card"  onMouseEnter={()=>onMouseEnter(index)} onMouseLeave={()=>onMouseLeaver(index)}>
+                  {/* <div  className="card"  onMouseEnter={()=>setIsHovered(true)} onMouseLeave={()=>setIsHovered(false)}> */}
                     {/* <h2>{index+1}</h2> */}
                     <img src={image} alt="" width="100px" />
                     <div className="details">
                       <h2>{name}</h2>
                       <h3>Current Price:₹{current_price}</h3>
                     </div>
+                    {hover && <span className='three-dots' onClick={clickedOnSpan} >
+                    <i className="fa-solid fa-ellipsis-vertical"></i>
+                    {showModal && <Modal/>}
+                    </span>
+                    }
                   </div>
-                </Link>
+                </div>
               );
             })}
       </div>
---
+++ src/components/Modal.tsx
@@ -0,0 +1,11 @@
+
+function Modal() {
+  return (
+    <div className="modal-container">
+        <label>Add to fav</label>
+        <label>Add to Analyst Later</label>
+    </div>
+  )
+}
+
+export default Modal
